acede16270ac209674f71fb9485ed041e7256385
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2023-05-14 12:53:33 +0530

    fix : add deploy template

--- buycoffee/coffeeContract/scripts/deploy.js
+++ buycoffee/coffeeContract/scripts/deploy.js
@@ -5,11 +5,7 @@ const main = async () => {
   console.log("Deploying contracts with account: ", deployer.address);
   console.log("Account balance: ", accountBalance.toString());
 
-  const Token = await hre.ethers.getContractFactory("CoffeePortal");
-  const portal = await Token.deploy({
-    value: hre.ethers.utils.parseEther("0.1"),
-  });
-  await portal.deployed();
+  // TODO : add deploy code
 
   console.log("CoffeePortal address: ", portal.address);
 };
